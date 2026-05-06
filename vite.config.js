0e7f15581dddf5b0449948c5a0f8c6e6b13967cc
Sal_4u <mrrvisal617@gmail.com>
2026-05-06 11:52:15 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,19 @@ import path from 'path';
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
   server: {
     port: 5173,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
+  define: {
+    "process.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
   },
 });
